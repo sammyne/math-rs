3655f355b0fefd1bb55d4c04b89c076944518439
sammyne <xiangminli@alumni.sjtu.edu.cn>
2022-08-29 05:15:35 +0000

    Deploying to gh-pages from @ sammyne/math-rs@216c780a0e317a7768020f0ff5d1666ea827f4f5 🚀

---
+++ implementors/core/marker/trait.Send.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["math"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.63.0/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> for <a class=\"struct\" href=\"math/big/struct.Int.html\" title=\"struct math::big::Int\">Int</a>","synthetic":true,"types":["math::big::int::Int"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var sourcesIndex = {};
+sourcesIndex["math"] = {"name":"","dirs":[{"name":"big","dirs":[{"name":"int","files":["mod.rs"]}],"files":["arith.rs","mod.rs","nat.rs"]},{"name":"bits","files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
