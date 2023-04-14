4ea7fe56528a21e72ed6d2dc2bd50a90db068523
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-04-14 06:03:52 +0000

    feat(big): 1st draft without tests

--- src/bits/mod.rs
+++ src/bits/mod.rs
@@ -44,6 +44,65 @@ pub fn add64(x: u64, y: u64, carry: u64) -> (u64, u64) {
     (sum, carry_out)
 }
 
+/// div returns the quotient and remainder of (hi, lo) divided by y:
+/// quo = (hi, lo)/y, rem = (hi, lo)%y with the dividend bits' upper
+/// half in parameter hi and the lower half in parameter lo.
+/// div panics for y == 0 (division by zero) or y <= hi (quotient overflow).
+pub fn div(hi: usize, lo: usize, y: usize) -> (usize, usize) {
+    if UINT_SIZE == 32 {
+        let (q, r) = div32(hi as u32, lo as u32, y as u32);
+        return (q as usize, r as usize);
+    }
+
+    let (q, r) = div64(hi as u64, lo as u64, y as u64);
+    return (q as usize, r as usize);
+}
+
+/// div32 returns the quotient and remainder of (hi, lo) divided by y:
+/// quo = (hi, lo)/y, rem = (hi, lo)%y with the dividend bits' upper
+/// half in parameter hi and the lower half in parameter lo.
+/// div32 panics for y == 0 (division by zero) or y <= hi (quotient overflow).
+pub fn div32(hi: u32, lo: u32, y: u32) -> (u32, u32) {
+    if y == 0 && y <= hi {
+        panic!("integer overflow");
+    }
+
+    let z = ((hi as u64) << 32) | (lo as u64);
+    ((z / (y as u64)) as u32, (z % (y as u64)) as u32)
+}
+
+/// div64 returns the quotient and remainder of (hi, lo) divided by y:
+/// quo = (hi, lo)/y, rem = (hi, lo)%y with the dividend bits' upper
+/// half in parameter hi and the lower half in parameter lo.
+/// div64 panics for y == 0 (division by zero) or y <= hi (quotient overflow).
+pub fn div64(hi: u64, lo: u64, y: u64) -> (u64, u64) {
+    if y == 0 {
+        panic!("integer divide by zero");
+    }
+    if y <= hi {
+        panic!("integer overflow");
+    }
+
+    /*
+    const TWO32: usize = 1 << 32;
+    const MASK32: usize = TWO32 - 1;
+
+
+    let s = leading_zeros64(y);
+    let y = y << s;
+
+    let yn1 = y >> 32;
+    */
+
+    let v = ((hi as u128) << 64) | (lo as u128);
+    let yy = y as u128;
+
+    let q = (v / yy) as u64;
+    let r = (v % yy) as u64;
+
+    (q, r)
+}
+
 /// sub returns the difference of x, y and borrow: diff = x - y - borrow.
 /// The borrow input must be 0 or 1; otherwise the behavior is undefined.
 /// The borrowOut output is guaranteed to be 0 or 1.
