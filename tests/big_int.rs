3b13fdd6326e861c5a0099f5bad51389bc38a6b4
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-04-21 01:21:35 +0000

    test(big): Int::sign

--- tests/big_int.rs
+++ tests/big_int.rs
@@ -2,7 +2,12 @@ use math::big::Int;
 
 lazy_static::lazy_static! {
   static ref SUM_ZZ: Vec<ArgZz> = vec![
-    ArgZz::new(Int::new(0), Int::new(0), Int::new(0))
+    ArgZz::new(Int::new(0), Int::new(0), Int::new(0)),
+    ArgZz::new(Int::new(1), Int::new(1), Int::new(0)),
+    ArgZz::new(Int::new(1111111110), Int::new(123456789), Int::new(987654321)),
+    ArgZz::from_i64s(-1, -1, 0),
+    ArgZz::from_i64s(864197532, -123456789, 987654321),
+    ArgZz::from_i64s(-1111111110, -123456789, -987654321),
   ];
 }
 
@@ -16,7 +21,22 @@ impl ArgZz {
     fn new(z: Int, x: Int, y: Int) -> Self {
         Self { z, x, y }
     }
+
+    fn from_i64s(z: i64, x: i64, y: i64) -> Self {
+        Self {
+            z: Int::new(z),
+            x: Int::new(x),
+            y: Int::new(y),
+        }
+    }
 }
 
 #[test]
-fn sign_z() {}
+fn sign_z() {
+    let zero = Int::default();
+    for a in SUM_ZZ.iter() {
+        let s = a.z.sign();
+        let e = a.z.cmp(&zero);
+        assert_eq!(s, e, "z = {}", a.z);
+    }
+}
