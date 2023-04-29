2de6392d1d39667997df108e56843305839bebd1
sammyne <xiangminli@alumni.sjtu.edu.cn>
2023-04-29 06:23:00 +0000

    fix(big): Int::set_string

--- tests/helper/mod.rs
+++ tests/helper/mod.rs
@@ -1,1 +1,10 @@
 pub mod rand;
+
+use math::big::Int;
+
+pub fn is_big_int_normalized(x: &Int) -> bool {
+    match x.bits().next_back() {
+        None => x.sign() == 0,
+        Some(v) => v != 0,
+    }
+}
